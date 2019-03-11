c02436d98731a6adaf6e6c23985ffca2ccb729cf
Administrator <jcsmnd@gmail.com>
2019-03-11 12:11:23 -0400

    hello world

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var app = express();
+//Written by Myungsik Kim 03/11/2019
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res) => res.send("welcome!"))
+app.listen(3100, () => console.log("server connected"))
+
+/* old version
 app.get('/', function(req, res){
     res.send("welcome!");
 });
 app.listen(3100, function(){
     console.log("server connected");
 });
+*/

0a8059eaaa9f7e03b286d5979c7ca3e2af22895e
Administrator <jcsmnd@gmail.com>
2019-03-16 15:45:46 -0400

    03-16-2019

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-//Written by Myungsik Kim 03/11/2019
+//Written by Myungsik Kim 03/16/2019 
 const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => res.send("welcome!"))
 app.listen(3100, () => console.log("server connected"))
-
-/* old version
-app.get('/', function(req, res){
-    res.send("welcome!");
-});
-app.listen(3100, function(){
-    console.log("server connected");
-});
-*/
